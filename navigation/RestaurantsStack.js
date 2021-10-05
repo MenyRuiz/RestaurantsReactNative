8f00e1ce3f398f97a910ca39095f70614e1819e2
Manuel Rincon <31856068+MenyRuiz@users.noreply.github.com>
2021-10-05 11:58:22 -0500

    Primer commit

---
+++ navigation/RestaurantsStack.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Restaurants from '../screens/Restaurants'
+
+const Stack = createStackNavigator()
+
+export default function RestaurantsStack() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+            headerShown: false
+          }}>
+            <Stack.Screen
+                name="restaurants"
+                component={Restaurants}
+                options={{ title: "Restaurantes Stack" }}
+                
+            />
+        </Stack.Navigator>
+    )
+}
---
+++ navigation/TopRestaurantsStack.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import TopRestaurants from '../screens/TopRestaurants'
+
+const Stack = createStackNavigator()
+
+export default function TopRestaurantsStack() {
+    return (
+        <Stack.Navigator
+        screenOptions={{
+            headerShown: false
+          }}>
+            <Stack.Screen
+                name="top-restaurants"
+                component={TopRestaurants}
+                options={{title: "Top"}}
+            />
+        </Stack.Navigator>
+    )
+}
